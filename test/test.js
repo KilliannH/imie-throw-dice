a128d2fde4408d1cee4fba5b82edc5d1e68cded6
KilliannH <killiannhervagault@gmail.com>
2019-05-22 16:30:06 +0200

    testing w. beforeach

--- test/test.js
+++ test/test.js
@@ -1,32 +1,46 @@
-var assert = require("assert");
-let chai = require("chai");
-let chaiHttp = require("chai-http");
-let server=require("../app");
-let should = chai.should();
+var expect = require('chai').expect;
 
+let numberOfDice , numberOfFaces;
 
+beforeEach(function (done) {
+    numberOfDice = 100;
+    numberOfFaces = 2;
+    done();
+});
 
-chai.use(chaiHttp);
-describe("API", function(){
-    describe ("/GET ALL", function(){
-        it("should not have permission", done => {
-            chai.request(server)
-                .get("/api/throws/")
-                .end((err,res) => {
-                    res.should.have.status(401);
-                    done()
-                });
+describe('Throws', function() {
+    describe('Number of dice not null && not above 100', function() {
+        it('should return a number > 0 and less or equal to 100', function(done) {
+            expect(numberOfDice).to.be.at.least(1);
+            expect(numberOfDice).not.to.be.above(100);
+            done();
         });
     });
+    describe('Number of faces between 2 and 100', function() {
+        it('should return a number >= 2 && <= 100', function(done) {
+            expect(numberOfFaces).to.be.at.least(2);
+            expect(numberOfFaces).not.to.be.above(100);
+            done();
+        });
+    });
+    describe('The sum is equal to the addition of the faces from all dices', function() {
+        it('should return a sum', function(done) {
+            let dicesface = [];
+            for(let i = 0; i < numberOfDice; i++) {
+                var min = 1;
+                var max = numberOfDice + 1;
 
-    describe ("/POST login", function(){
-        it("should login", done => {
-            chai.request(server)
-                .get("/api/throws/")
-                .end((err,res) => {
-                    res.should.have.status(401);
-                    done()
-                });
+                var random = parseInt(Math.random() * (+max - +min) + +min);
+
+                dicesface.push(random);
+            }
+
+            let result = 0;
+            for(let i = 0; i < dicesface.length; i++) {
+                result += dicesface[i];
+            }
+            expect(result).to.be.a('number');
+            done();
         });
     });
 });
