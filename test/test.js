aa969f0d3290136dd5a9c05cd85214c3bfe378dc
KilliannH <killiannhervagault@gmail.com>
2019-05-22 11:50:59 +0200

    part 2 tests added

--- test/test.js
+++ test/test.js
@@ -2,16 +2,37 @@
 var expect = require('chai').expect;
 
 describe('Throws', function() {
-    let numberOfDice = 2;
+    let numberOfDice = 100;
     let numberOfFaces = 2;
-    describe('Number of dice not null', function() {
-        it('should return an error if number of dice is less or equal to 0', function() {
+    describe('Number of dice not null && not above 100', function() {
+        it('should return a number > 0 and less or equal to 100', function() {
             expect(numberOfDice).to.be.at.least(1);
+            expect(numberOfDice).not.to.be.above(100);
         });
     });
-    describe('Number of faces not null', function() {
-        it('should return an error if number of faces is less or equal to 0', function() {
-            expect(numberOfFaces).to.be.at.least(1);
+    describe('Number of faces between 2 and 100', function() {
+        it('should return a number >= 2 && <= 100', function() {
+            expect(numberOfFaces).to.be.at.least(2);
+            expect(numberOfFaces).not.to.be.above(100);
+        });
+    });
+    describe('The sum is equal to the addition of the faces from all dices', function() {
+        it('should return a sum', function() {
+            let dicesface = [];
+            for(let i = 0; i < numberOfDice; i++) {
+                var min = 1;
+                var max = numberOfDice + 1;
+
+                var random = parseInt(Math.random() * (+max - +min) + +min);
+
+                dicesface.push(random);
+            }
+
+            let result = 0;
+            for(let i = 0; i < dicesface.length; i++) {
+                result += dicesface[i];
+            }
+            expect(result).to.be.a('number');
         });
     });
 });
